ea09282c90e4342006f8eb3ac8a6322134c9e7a4
Jesse Vandenkerckhove <jesse.vandenkerckhove@student.howest.be>
2019-11-06 15:46:30 +0100

    changed class structure

--- resources/src/Controllers/UserController.php
+++ resources/src/Controllers/UserController.php
@@ -1,7 +1,24 @@
 <?php
-
+include_once '../Models/User.php';
 
 class UserController
 {
+
+    public function login($name, $password) {
+        $name = trim($name);
+        $password = trim($password);
+        $user = User::getByName($name);
+        if($user->getEnabled() || password_verify($password, $user->getPassword())) {
+            $this->registerLoginSession($user);
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function registerLoginSession($user) {
+        if(session_status() == PHP_SESSION_ACTIVE) {
+
+        }
+    }
 
 }
--- resources/src/Models/Session.php
+++ resources/src/Models/Session.php
@@ -3,7 +3,69 @@
 
 class Session
 {
+    const DELETESESSIONSOFUSER = 'DELETE FROM user_sessions WHERE (user_id = :user_id)';
+    const REPLACESESSION = 'REPLACE INTO sessions (id, user_id, login_time) VALUES (:id, :user_id, NOW())';
+
     private $id;
     private $user_id;
     private $login_time;
+
+    public static function replace($session_id, $user_id) {
+        $query = self::REPLACESESSION;
+        $values = array(':id' => $session_id, ':user_id' => $user_id);
+        try {
+            $res = Database::getInstance()->conn->prepare($query);
+            $res->execute($values);
+        } catch (PDOException $e) {
+            throw new Exception('Database query error');
+        }
+    }
+
+    public static function deleteByUserId($userid) {
+        $query = self::DELETESESSIONSOFUSER;
+        $values = array(":user_id" => $userid);
+
+        try {
+            $res = Database::getInstance()->conn->prepare($query);
+            $res->execute($values);
+        } catch(PDOException $e) {
+            throw new Exception('Database query error');
+        }
+    }
+
+
+
+
+
+
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLoginTime()
+    {
+        return $this->login_time;
+    }
+
+
+
+
 }
