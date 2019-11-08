0a9d314254877a34751e4bc9032b054071781ede
leonardql <leonard.qoli@hotmail.com>
2019-11-08 14:57:10 +0100

    added bootsrap css en js file

--- public_html/wizard.php
+++ public_html/wizard.php
@@ -3,11 +3,14 @@ include_once("../resources/config.php");
 
 $pageTitle="Thermoloket";
 $cssFiles=["wizard.css"];
+$cssFiles=["bootstrap.min.css"];
 
 
 include_once("../resources/src/Views/general/htmlStartView.php");
 include_once("../resources/src/Views/wizardView.php");
 include_once("../resources/src/Views/general/htmlEndView.php");
 
-
+$jsFiles=["wizard.js"];
+$jsFiles=["jquery-3.4.1.min.js"];
+$jsFiles=["bootstrap.min.js"];
 ?>
