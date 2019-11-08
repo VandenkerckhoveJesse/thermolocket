874a0c8bd742a4c2b771b3e57222b5201d6bffeb
Vincent Elias <vincent.elias@hotmail.com>
2019-11-08 12:41:36 +0100

    Added view for userlist in admin-panel

--- resources/src/Views/admin/usersView.php
+++ resources/src/Views/admin/usersView.php
@@ -14,114 +14,39 @@
           </nav>
         </div>
         <div class="col-8 col-lg-10 p-4">
-          <div class="row">
-            <div class="col-12 col-lg-6">
-              <h2 class="username mb-4">Johndoe</h2>
-              <form
-                class="edit-user-details"
-                action="/update-user"
-                method="POST"
-              >
-                <label for="username">Gebruikersnaam aanpassen:</label>
-                <input class="d-block w-50" type="text" id="username" />
+          <h2 class="user-count mb-5">20</h2>
 
-                <label for="username">Email aanpassen:</label>
-                <input class="mb-3 w-50" type="text" id="username" />
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Gebruikersnaam</th>
+                <th scope="col">Laatste login</th>
+                <th scope="col">Acties</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Johndoe</td>
+                <td>5/11/19</td>
+                <td><a href="edit-user" class="button">Beheren</a></td>
+              </tr>
 
-                <label for="password">Wachtwoord aanpassen:</label>
-                <input class="mb-3" type="password" id="password" placeholder="Wachtwoord" />
-                <input
-                  class="mb-3"
-                  type="password"
-                  id="repeat-password"
-                  placeholder="Herhaal wachtwoord"
-                />
+              <tr>
+                <th scope="row">1</th>
+                <td>Johndoe</td>
+                <td>5/11/19</td>
+                <td><a href="edit-user" class="button">Beheren</a></td>
+              </tr>
 
-                <label for="status">Status: </label>
-                <input
-                  type="radio"
-                  name="status"
-                  value="enabled"
-                  id="status-enabled"
-                />
-                <label class="d-inline-block mr-2" for="status-enabled"
-                  >Enabled</label
-                >
-                <input
-                  type="radio"
-                  name="status"
-                  value="disabled"
-                  id="status-disabled"
-                />
-                <label class="d-inline-block mb-4" for="status-disabled"
-                  >Disabled</label
-                >
-
-                <input class="mb-4 d-block" type="submit" class="d-block" value="Opslaan" />
-              </form>
-            </div>
-            <div class="col-12 col-lg-6">
-              <h2 class="mb-4">Gemeentes</h2>
-              <p>Deze gebruiker heeft toegang tot volgende gemeentes:</p>
-              <form action="#">
-                <input
-                  class="mb-2"
-                  type="text"
-                  name="search"
-                  placeholder="gemeente zoeken"
-                />
-              </form>
-              <ul class="current-towns mb-3">
-                <li id="8000"><a>Brugge (8000)</a></li>
-                <li id="8490"><a>a Jabbeke (8490)</a></li>
-                <li id="8370"><a>Blankenberge (8370)</a></li>
-                <li id="8420"><a>De Haan (8420)</a></li>
-                <li id="9031"><a>Drongen (9031)</a></li>
-                <li id="3501"><a>Wimmertingen (3501)</a></li>
-                <li id="8000"><a>Brugge (8000)</a></li>
-                <li id="8490"><a>Jabbeke (8490)</a></li>
-                <li id="8370"><a>Blankenberge (8370)</a></li>
-                <li id="8420"><a>De Haan (8420)</a></li>
-                <li id="9031"><a>Drongen (9031)</a></li>
-                <li id="3501"><a>Wimmertingen (3501)</a></li>
-              </ul>
-
-              <button class="mb-4">Opslaan</button>
-
-              <h2>Gemeentes Toevoegen</h2>
-              <form action="#">
-                <input
-                  class="mb-2"
-                  type="text"
-                  name="search"
-                  placeholder="gemeente zoeken"
-                />
-              </form>
-
-              <ul>
-                <li>
-                  <input
-                    type="checkbox"
-                    name="brugge-8000"
-                    id="brugge-8000"
-                    value="brugge-8000"
-                  />
-                  <label for="brugge-8000">Brugge (8000)</label>
-                </li>
-
-                <li>
-                  <input
-                    type="checkbox"
-                    name="hasselt-3500"
-                    id="hasselt-3500"
-                    value="hasselt-3500"
-                  />
-                  <label for="hasselt-3500">Hasselt (3500)</label>
-                </li>
-              </ul>
-              <button>Voeg geselecteerde gemeentes toe</button>
-            </div>
-          </div>
-        </div>
+              <tr>
+                <th scope="row">1</th>
+                <td>Johndoe</td>
+                <td>5/11/19</td>
+                <td><a href="edit-user" class="button">Beheren</a></td>
+              </tr>
+            </tbody>
+          </table>
       </div>
     </div>
